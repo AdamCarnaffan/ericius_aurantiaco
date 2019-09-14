f6feff862ade018935477fe111124d4e069f7992
GalacticGlum <nerftactic09@gmail.com>
2019-09-14 16:28:45 -0400

    Added metric status to the news card component

--- webapp/src/components/NewsCard.js
+++ webapp/src/components/NewsCard.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import {
   Col, Row, Button,
-  Card, CardImg, CardText, CardBody, CardTitle,
-  Popover, PopoverHeader, PopoverBody
+  Card, CardImg, CardText, CardBody, CardTitle
 } from 'reactstrap';
 import Truncate from 'react-truncate';
+import lerpColour from 'color-interpolate';
 import './NewsCard.css';
 
 function Thumbnail(props) {
@@ -19,27 +19,13 @@ function Thumbnail(props) {
 }
 
 export default class NewsCard extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggleScorePopover = this.toggleScorePopover.bind(this);
-    this.state = {
-      scorePopoverOpen: false
-    };
-  }
-
-  toggleScorePopover() {
-    this.setState({
-      scorePopoverOpen: !this.state.scorePopoverOpen
-    });
-  }  
-
   render() {
+    const statusColour = lerpColour(['#d9534f', '#f0ad4e', '#28a745'])(this.props.honestyMetric / 100);
     return (
         <Card className="shadow border-0">
           <Row>
             <Col md="9" className="order-12">
-            <CardBody>
+            <CardBody className="callout" style={{borderLeftColor: statusColour}}>
               <strong className={"mb-2 " + this.props.source.className}>{this.props.source.name}</strong>
               <CardTitle className="mb-0 w-100" tag="h3">
                 {this.props.title}
@@ -53,29 +39,13 @@ export default class NewsCard extends Component {
             </Col>
             <Col md="3" className="order-1 order-md-12 card-thumbnail-col">
               <Thumbnail image={this.props.thumbnail} />
+              <div className="news-card-score-overlay">
+                <Button size="md" style={{background: statusColour, borderColor: statusColour}} className="m-3 news-card-score-button">
+                  <strong>{this.props.honestyMetric}</strong>
+                </Button>
+              </div>
             </Col>
           </Row>
-          
-          {/* <div className="featured-card-thumbnail">
-            <Thumbnail image={this.props.thumbnail} />            
-            <div className="news-card-score-overlay">
-              <Button size="lg" color="success" id="scoreButton"
-                className="news-card-score-button m-3 rounded-circle">
-                <strong>51</strong>
-              </Button>
-              <Popover placement="left" isOpen={this.state.scorePopoverOpen} 
-                target="scoreButton" toggle={this.toggleScorePopover} trigger="focus">
-                <PopoverHeader>Score Breakdown</PopoverHeader>
-                <PopoverBody>
-                  <ul className="pl-3">
-                    <li>Citations</li>
-                    <li>Relevancy</li>
-                    <li>Lack of Author</li>
-                  </ul>
-                </PopoverBody>
-              </Popover>
-            </div>
-          </div> */}
         </Card>
     )
   }
