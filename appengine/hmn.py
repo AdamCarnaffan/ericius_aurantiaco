0bda8aece602b3b4082b54cf2e931aac32f4d3d8
Adam Carnaffan <acarnaffan@hotmail.com>
2019-09-15 02:27:43 -0400

    Database fix

--- appengine/hmn.py
+++ appengine/hmn.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager import chrome
 from json import loads, dumps
 from apscheduler.schedulers.background import BackgroundScheduler
-from firebase_admin import db, credentials, firestore
+from firebase_admin import credentials, firestore
 import firebase_admin
 import requests
 import time
@@ -23,9 +23,6 @@ cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 
 
-conn = firestore.client().collection('articles')
-
-
 class Site_Data:
 
     def __init__( self, url):
@@ -34,9 +31,12 @@ class Site_Data:
 
     def process_article(self):
         # Get Schema
-        schemaSearch = self.soup.find('script', text=re.compile('schema.org'))
-        print(re.sub(r'\\x..', '', re.sub('^[^{]*', '', re.sub('[^}]*$', '', schemaSearch.string)).replace("\\'", "'")))
-        self.schema = loads(re.sub(r'\\x..', '', re.sub('^[^{]*', '', re.sub('[^}]*$', '', schemaSearch.string)).replace("\\'", "'").replace('\\n', ''))) if schemaSearch is not None else None
+        try:
+            schemaSearch = self.soup.find('script', text=re.compile('schema.org'))
+            print(re.sub(r'\\x..', '', re.sub('^[^{]*', '', re.sub('[^}]*$', '', schemaSearch.string)).replace("\\'", "'")))
+            self.schema = loads(re.sub(r'\\x..', '', re.sub('^[^{]*', '', re.sub('[^}]*$', '', schemaSearch.string)).replace("\\'", "'").replace('\\n', '').replace('\\\\"', ''))) if schemaSearch is not None else None
+        except:
+            self.schema = None
         # Get title
         title = self.soup.title
         self.title = title.string if title is not None else None
@@ -77,7 +77,10 @@ class Site_Data:
                     largest[1] = len(popImages[key])
             self.images = []
             for img in popImages[largest[0]]:
-                self.images += [[img['src'], img.get('alt')]]
+                try:
+                    self.images += [[img['src'], img.get('alt')]]
+                except:
+                    pass
         else:
             self.images = []
         # Fetch video info
@@ -320,12 +323,16 @@ def test():
 
 @app.route("/request", methods=['POST'])
 def get_page_data(url=None):
+    # print(firestore.client().collection('articles').document('url').get())
+    print(firestore.client().collection('articles').document(u'NAuT0kNfQvLQFlila3AT').get().to_dict())
+    return ""
     if url is None:
         url = request.form.get('url')
+        if url is None: return "An error occured loading the URL"
     newdt = Site_Data(url)
     newdt.process_article()
     newdt.total_rating()
-    return str(newdt.rating)
+    return str(newdt.rating) + " - O {}, M {}, C {}".format(newdt.opinionScore, newdt.mediaScore, newdt.citationScore)
 
 # Maintenance
 
