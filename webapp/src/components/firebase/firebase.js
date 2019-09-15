04b9480bc1bb6ac034f0ac4a1c3a5e8968af87eb
GalacticGlum <nerftactic09@gmail.com>
2019-09-15 00:53:56 -0400

    Staging firebase * Not working atm

--- webapp/src/components/firebase/firebase.js
+++ webapp/src/components/firebase/firebase.js
@@ -1,7 +1,8 @@
-import firebase from 'firebase'
+import app from 'firebase/app';
+import 'firebase/database';
 
 // Your web app's Firebase configuration
-let firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDNq7q5reVP8daTySkuwoOGeeNfChmtrzo",
     authDomain: "honesty-matters-news.firebaseapp.com",
     databaseURL: "https://honesty-matters-news.firebaseio.com",
@@ -10,7 +11,11 @@ let firebaseConfig = {
     messagingSenderId: "162246276993",
     appId: "1:162246276993:web:4dd64915b3a09b5442da80"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-export default firebase;
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+    this.db = app.database();
+  }
+}
+export default Firebase;
