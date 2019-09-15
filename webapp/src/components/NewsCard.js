06216f3f9a031509f4e388e82f089a963d83977c
GalacticGlum <nerftactic09@gmail.com>
2019-09-14 20:59:22 -0400

    Staging metric status breakdown

--- webapp/src/components/NewsCard.js
+++ webapp/src/components/NewsCard.js
@@ -11,18 +11,48 @@ import { faSmileBeam, faSmile, faMeh, faFrown, faSadCry } from '@fortawesome/fre
 import './NewsCard.css';
 
 function MetricStatusButton(props) {
-  const shadowClass = 'shadow' in props && props.shadow ? 'shadow' : '';
   return (
     <div className="news-card-score-overlay">
-        <Button size="md" style={{background: props.statusColour, borderColor: props.statusColour}} 
-          className={"m-3 news-card-score-button " + shadowClass} onClick={props.onStatusButtonClicked}>
-            <div className="w-100 h-100 align-self-center">
-              <FontAwesomeIcon icon={props.statusIcon} className="mr-2" />
-              <strong>{props.honestyMetric}</strong>
-            </div>
-        </Button>
+      <MetricStatusButtonInner statusColour={props.statusColour} onStatusButtonClicked={props.onStatusButtonClicked}
+        statusIcon={props.statusIcon} honestyMetric={props.honestyMetric} />
     </div>
   );
+}
+
+function MetricStatusButtonInner(props) {
+  const shadowClass = 'shadow' in props && props.shadow ? 'shadow' : '';
+  return (
+    <Button size="md" style={{background: props.statusColour, borderColor: props.statusColour}} 
+      className={"m-3 news-card-score-button " + shadowClass + " " + props.className} onClick={props.onStatusButtonClicked}>
+        <div className="w-100 h-100 align-self-center">
+          <FontAwesomeIcon icon={props.statusIcon} className="mr-2" />
+          <strong>{props.honestyMetric}</strong>
+        </div>
+    </Button>
+  );
+}
+
+function GetMetricStatusIcon(metric) {
+    // Determine the status icon.
+    let statusIcon = faSadCry;
+    if (metric >= 90) {
+      // If honesty metric is between 100 and 90, use the faSmileBeam icon.
+      statusIcon = faSmileBeam;
+    }
+    // Use the smile icon in the [70, 90) interval.
+    if (metric < 90 && metric >= 70) {
+      statusIcon = faSmile;
+    }
+    // Use the meh icon in the [40, 70) interval.
+    else if (metric < 70 && metric >= 40) {
+      statusIcon = faMeh;
+    }
+    // Use the frown icon in the [20, 40) interval.
+    else if (metric < 40 && metric >= 20) {
+      statusIcon = faFrown;
+    }
+
+    return statusIcon;
 }
 
 function Thumbnail(props) {
@@ -67,35 +97,21 @@ export default class NewsCard extends Component {
     else {
       statusColour = lerpColour(['#f0ad4e', '#50a847', '#28a745'])(t / 2);
     }
-    
-    // Determine the status icon.
-    let statusIcon = faSadCry;
-    if (this.props.honestyMetric >= 90) {
-      // If honesty metric is between 100 and 90, use the faSmileBeam icon.
-      statusIcon = faSmileBeam;
-    }
-    // Use the smile icon in the [70, 90) interval.
-    if (this.props.honestyMetric < 90 && this.props.honestyMetric >= 70) {
-      statusIcon = faSmile;
-    }
-    // Use the meh icon in the [40, 70) interval.
-    else if (this.props.honestyMetric < 70 && this.props.honestyMetric >= 40) {
-      statusIcon = faMeh;
-    }
-    // Use the frown icon in the [20, 40) interval.
-    else if (this.props.honestyMetric < 40 && this.props.honestyMetric >= 20) {
-      statusIcon = faFrown;
-    }
+  
+    const statusIcon = GetMetricStatusIcon(this.props.honestyMetric);
+    const userStatusIcon = GetMetricStatusIcon(this.props.userHonestyMetric);
 
     const rippleAnimationClass = this.state.showMetricBreakdown ? 'animate-ripple' : '';
     const rippleOverlayBlockEventsClass = this.state.showMetricBreakdown ? 'block-events' : '';
     const metricBreakdownAnimationClass = this.state.showMetricBreakdown ? 'animate-metric-breakdown-content' : '';
+    const userMetricStatusAnimationClass = this.state.showMetricBreakdown ? 'animate-user-metric-status' : '';
 
     return (
       <Card className="news-card shadow border-0">
         <Row>
           <Col md="9" className="order-12">
-            <CardBody className="callout" style={{borderLeftColor: statusColour}}>
+            <CardBody className={"callout " + rippleOverlayBlockEventsClass}
+               style={{borderLeftColor: statusColour}}>
               <strong className="mb-2 text-secondary">{this.props.source.name}</strong>
               <CardTitle className="news-card-title mb-0 w-100" tag="h3" onClick={this.onCardClick}>
                 {this.props.title}
@@ -113,10 +129,24 @@ export default class NewsCard extends Component {
               onStatusButtonClicked={this.onStatusButtonClicked} honestyMetric={this.props.honestyMetric} shadow />
           </Col>
         </Row>
-        <Row className={"ripple-overlay " + rippleOverlayBlockEventsClass}>
+        <Row className="ripple-overlay">
           <div className={"ripple " + rippleAnimationClass} style={{background: statusColour}} />
-          <MetricStatusButton statusColour={statusColour} statusIcon={statusIcon}
+          <div className="ripple-overlay w-100">
+            <Row>
+              <Col className="d-flex flex-row justify-content-end">           
+                <MetricStatusButtonInner statusColour={statusColour} statusIcon={statusIcon}
               onStatusButtonClicked={this.onStatusButtonClicked} honestyMetric={this.props.honestyMetric} />
+              </Col>
+              <div className="w-100"></div>
+              <Col className="d-flex flex-row justify-content-end">           
+                <MetricStatusButtonInner statusColour={statusColour} statusIcon={userStatusIcon} 
+                  honestyMetric={this.props.userHonestyMetric} 
+                  className={"user-metric-status " + userMetricStatusAnimationClass} />
+              </Col>
+            </Row>
+          </div>
+
+
           <Card className={"ripple-overlay w-100 metric-breakdown-content " + metricBreakdownAnimationClass}>
             <CardBody>
               <CardTitle className="news-card-title mb-0 w-100" tag="h5">
