b4c3ea7ce627ab65b7eff6b47de940862ca1c03f
GalacticGlum <nerftactic09@gmail.com>
2019-09-15 04:54:48 -0400

    Added database integration

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,25 +2,73 @@ import React, { Component } from 'react';
 import './App.css';
 import { Container } from 'reactstrap';
 import NewsCard from './components/NewsCard';
+import moment from 'moment';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.setState({
+      articles: null
+    });
+  }
+  
+  componentDidMount() {
+    let articles = [];
+    fetch('https://honesty-matters-news.appspot.com/pleasegivemedatamylordplease', {method: 'post'})
+      .then(response => response.json())
+      .then(responseJson => {
+        for (let i = 0; i < responseJson.length; ++i) {
+          const article = {
+            citationScore: responseJson[i]['citation_score'],
+            headline: responseJson[i]['headline'],
+            mediaScore: responseJson[i]['media_score'],
+            opinionScore: responseJson[i]['opinion_score'],
+            rating: responseJson[i]['rating'],
+            thumbnail: responseJson[i]['thumbnail'],
+            timestamp: responseJson[i]['timestamp'],
+            url: responseJson[i]['url']
+          };
+  
+          articles.push(article);
+        }
+
+        articles.sort((a, b) => new Date(b.timestamp) >= new Date(a.timestamp) ? 1 : -1);
+        this.setState({
+          articles: articles
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        this.setState({
+          articles: null
+        });
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
+    if (this.state === null) return null;
+    const articleCards = this.state.articles.map((article) => {
+      return (
         <Container className="app-container py-3">
-          <NewsCard source={{ name: "CNN", className: "text-danger" }} honestyMetric={50} userHonestyMetric={70}
-            title="New York AG exposes $1 billion in wire transfers by Sackler family"
-            content="The state attorney general's office is trying to find how much money the owners of pharmaceutical giant Purdue Pharma amassed and where it is."
-            thumbnail={{ source: "https://cdn.cnn.com/cnnnext/dam/assets/190312114823-20190312-opioid-v10-large-tease.jpg", alt: "Thumbnail" }}
-            url="https://www.cnn.com/2019/09/13/us/sackler-family-purdue-pharma-case/index.html"
+          <NewsCard source={{ name: moment(article.timestamp).format('MMMM Do YYYY, h:mm:ss a') }}
+            honestyMetric={article.rating} userHonestyMetric={70}
+            title={article.headline}
+            content={"test"}
+            thumbnail={{ source: article.thumbnail, alt: "Thumbnail" }}
+            url={article.url}
             breakdownFactors={[
-              { name: "Presence of Citations", value: 100 },
-              { name: "Objectivity", value: 96 },
-              { name: "Image Caption References", value: 83 },
+              { name: "Presence of Citations", value: article.citationScore },
+              { name: "Objectivity", value: article.opinionScore },
+              { name: "Media Score", value: article.mediaScore},
               { name: "Author Reliability", value: 86 },
               { name: "Site Reliability", value: 100 },
             ]} />
         </Container>
+      );
+    });
+    return (
+      <div className="App">
+        {articleCards}
       </div>
     );
   }
